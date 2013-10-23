54b1b8a0a5e0c277dca8bd5585979720c225bf01
unknown <nilgot@D211.sedo.de.intern>
2013-10-23 15:47:39 +0200

    Added new implementation of callback for sql

---
+++ src/php/Lousson/Config/Callback/CallbackConfigSQL.php
@@ -0,0 +1,360 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 textwidth=75: *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2012 - 2013, The Lousson Project                        *
+ *                                                                       *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * Redistribution and use in source and binary forms, with or without    *
+ * modification, are permitted provided that the following conditions    *
+ * are met:                                                              *
+ *                                                                       *
+ * 1) Redistributions of source code must retain the above copyright     *
+ *    notice, this list of conditions and the following disclaimer.      *
+ * 2) Redistributions in binary form must reproduce the above copyright  *
+ *    notice, this list of conditions and the following disclaimer in    *
+ *    the documentation and/or other materials provided with the         *
+ *    distribution.                                                      *
+ *                                                                       *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   *
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     *
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     *
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE        *
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,            *
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR    *
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)    *
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,   *
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)         *
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED   *
+ * OF THE POSSIBILITY OF SUCH DAMAGE.                                    *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ *  Lousson\Config\Callback\CallbackConfigSQL class definition
+ *
+ *  @package    org.lousson.config
+ *  @copyright  (c) 2012 - 2013, The Lousson Project
+ *  @license    http://opensource.org/licenses/bsd-license.php New BSD License
+ *  @author     Nils Gotzhein <nils.gotzhein at gmail.com>
+ *  @filesource
+ */
+namespace Lousson\Config\Callback;
+
+/** Dependencies: */
+use Lousson\Config\AbstractConfig;
+use Lousson\Config\AnyConfigEntity;
+use Closure;
+
+/** Exceptions: */
+use Lousson\Config\Error\ConfigRuntimeError;
+
+/**
+ *  A Closure-based implementation of the AnyConfigEntity interface
+ *
+ *  The Lousson\Config\Callback\CallbackConfigSQL is a flexible implementation
+ *  of the AnyConfigEntity interface, using a Closure to retrieve config values.
+ *  
+ *  This implementation communicates with the closure via SQL, enabling storage
+ *  of the configuration data in a SQL compatible storage engine.
+ *
+ *  @since      lousson/Lousson_Config-0.2.0
+ *  @package    org.lousson.config
+ */
+class CallbackConfigSQL
+	extends AbstractConfig
+	implements AnyConfigEntity
+{
+    /**
+     *  Create a config instance
+     *
+     *  The constructor allows to pass a Closure $getter that is used to
+     *  retrieve configuration values. This callback must provide the exact
+     *  same interface as the getOption() method, otherwise the behavior
+     *  is undefined.
+     *
+     *  @param  Closure             $callback 		The config callback
+     *  @param	string				$tableName		The name of the SQL table
+     *  											to use in all outgoing statements
+     *  @param	string				$keyField		The name of the column representing
+     *  											the property name
+     *  @param	string				$valueField		The name of the column containing
+     *  											the values
+     *  @param	array (optional)	$index			array containing additional key/value
+     *  											restrictions to apply to the WHERE-
+     *  											part of all outgoing SQL queries
+     *  											Expects column names as keys and
+     *  											their required values as array values
+     */
+	public function __construct(
+		Closure $callback,
+		$tableName,
+		$keyField,
+		$valueField,
+		array $index = array()
+	) {
+		
+		$this->generateSql( $valueField, $tableName, $index, $keyField );
+		
+		$this->callback = $callback;
+        
+    }
+
+    /**
+     *  Update the value of a particular option
+     *
+     *  The setOption() method is used to assign the given $value to the
+     *  option identified by the given $key.
+     *
+     *  @param  string              $name           The option name
+     *  @param  mixed               $value          The option value
+     *
+     *  @throws \Lousson\Config\AnyConfigException
+     *          Raised in case the $name or $value is malformed
+     */
+    public function setOption($name, $value)
+    {
+
+    	if( is_array( $value ) || null === $value ) {
+    		$value = self::JSON_INDICATOR . json_encode($value);
+    	}
+    	
+    	$exists = $this->hasOption($name);
+    	
+    	$stack = $exists ? $this->sql_update : $this->sql_insert;
+		$stack[1] = $value;
+		$stack[] = $name;
+
+		try {
+			call_user_func_array($this->callback, $stack);
+    	}
+		catch (\Exception $error) {
+            $class = get_class($error);
+            $message = "Could not write option: Caught $class";
+            $code = $error->getCode();
+            throw new ConfigRuntimeError($message, $code, $error);
+        }
+
+    }
+    
+    /**
+     *  Obtain the value of a particular option
+     *
+     *  The getOption() method will return the value associated with the
+     *  option identified by the given $name. If there is no such option,
+     *  it will either return the $fallback value - if provided -, or
+     *  raise an exception.
+     *
+     *  @param  string              $name           The option name
+     *  @param  mixed               $fallback       The fallback value
+     *
+     *  @return mixed
+     *          The value of the option is returned on success
+     *
+     *  @throws \Lousson\Config\AnyConfigException
+     *          Raised in case of any error
+     *
+     *  @link   http://php.net/manual/en/function.func-num-args.php
+     *  @link   http://php.net/manual/en/language.functions.php
+     */
+    public function getOption($name, $fallback = null)
+    {
+        
+    	$name = $this->normalizeName($name, "retrieve");
+        $stack = $this->sql_select;
+		$stack[] = $name;
+		$useFallback = null !== $fallback;
+
+        try {
+            $value = call_user_func_array($this->callback, $stack);
+        }
+        catch (\Exception $error) {
+            $class = get_class($error);
+            $message = "Could not retrieve option: Caught $class";
+            $code = $error->getCode();
+            throw new ConfigRuntimeError($message, $code, $error);
+        }
+        $value = $this->normalizeValue($value, "retrieve");
+        if (null === $value) {
+        	if($useFallback) {
+        		$value = $fallback;
+        	}
+        	else {
+        		$message = "Could not retrieve unknown option: $name";
+        		throw new ConfigRuntimeError($message);
+        	}
+        }
+		elseif( 0 === strpos( $value, self::JSON_INDICATOR ) ) {
+        	$value = json_decode( substr( $value, strlen( self::JSON_INDICATOR ) ), true );
+        }
+
+        return $value;
+    }
+
+    /**
+     *  Check whether a particular option exists
+     *
+     *  The hasOption() method will return TRUE in case a subsequent call
+     *  to getOption() would succeed, when the same $name but no $fallback
+     *  is provided. FALSE will be returned otherwise.
+     *
+     *  @param  string      $name       The name of the option to check
+     *
+     *  @return boolean
+     *          TRUE is returned if the option exists, FALSE otherwise
+     */
+    public function hasOption($name)
+    {
+    	try {
+    		$name = $this->normalizeName($name, "retrieve");
+	        $stack = $this->sql_select;
+			$stack[] = $name;
+            $value = call_user_func_array($this->callback, $stack);
+           	$result = false;
+            if(null !== $value) {
+            	$value = $this->normalizeValue($value, "retrieve");
+            	$result = true;
+            }
+        }
+        catch (\Exception $error) {
+            $result = false;
+        	trigger_error("Caught $error", E_USER_WARNING);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Internal function for transcribng the input data into SQL queries
+     * 
+     * The generateSql method will fill private properties of this class
+     * with SQL statements for SELECT, INSERT, UPDATE and delete. This is
+     * done in preparation for later read/write operations invoked by the
+     * set/get/hasOption methods.
+     * 
+     * @param string $valueField	The name of the column containing
+     *  							the values
+     * @param string $tableName		The name of the SQL table
+     *  							to use in all outgoing statements
+     * @param array $index			array containing additional key/value
+     *  							restrictions to apply to the WHERE-
+     *  							part of all outgoing SQL queries
+     *  							Expects column names as keys and
+     *  							their required values as array values
+     * @param string $keyField		The name of the column representing
+     *  							the property name
+     */
+	private function generateSql( $valueField, $tableName, $index, $keyField ) {
+		
+		$sql_select = array(
+			"SELECT {$valueField} FROM {$tableName} WHERE"
+		);
+		$sql_insert = array(
+			"INSERT INTO {$tableName} SET {$valueField} = %s"
+		);
+		$sql_update = array(
+			"UPDATE {$tableName} SET {$valueField} = %s WHERE"
+		);
+		$sql_delete = array(
+			"DELETE FROM {$tableName} WHERE"
+		);
+		
+		$stack = array();
+		
+		//fill where with key-value pairs from $index array
+		foreach ($index as $key => $value) {
+			$fragment = "{$key} = %s AND";
+			$sql_select[] = $fragment;
+			$sql_insert[] = $fragment;
+			$sql_update[] = $fragment;
+			$sql_delete[] = $fragment;
+			$stack[] = $value;
+		}
+		
+		//fill where with key-value pairs from $index array
+		$fragment = "{$keyField} = %s\n";
+		$sql_select[] = $fragment;
+		$sql_insert[] = $fragment;
+		$sql_update[] = $fragment;
+		$sql_delete[] = $fragment;
+		
+		$this->sql_select = array_merge(
+			array(implode(" ", $sql_select)),
+			$stack
+		);
+		$this->sql_insert = array_merge(
+			array(implode(" ", $sql_insert), NULL),
+			$stack
+		);
+		$this->sql_update = array_merge(
+			array(implode(" ", $sql_update), NULL),
+			$stack
+		);
+		$this->sql_delete = array_merge(
+			array(implode(" ", $sql_delete), NULL),
+			$stack
+		);
+		
+	}
+	
+	/**
+	 * Constant holing the internaly used prefix for storing multi-
+	 * dimensional data in a SQL data field. The prefix is appended
+	 * to field-content which contains json-encoded data. This way
+	 * it can be identified whether the data is json encoded or not.
+	 * 
+	 * @var string
+	 */
+	CONST JSON_INDICATOR = 'json:';
+	
+    /**
+     * Internal property holding the SQL query for SELECT operations
+     * used in getOption and hasOption methods. This property is filled
+     * by internal method "generateSql".
+     * 
+     * @var string
+     */
+    private $sql_select;
+    
+    /**
+     * Internal property holding the SQL query for INSERT operations
+     * used in getOption and hasOption methods. This property is filled
+     * by internal method "generateSql".
+     * 
+     * @var string
+     */
+    private $sql_insert;
+    
+    /**
+     * Internal property holding the SQL query for UPDATE operations
+     * used in getOption and hasOption methods. This property is filled
+     * by internal method "generateSql".
+     * 
+     * @var string
+     */
+    private $sql_update;
+    
+    /**
+     * Internal property holding the SQL query for DELETE operations
+     * used in getOption and hasOption methods. This property is filled
+     * by internal method "generateSql".
+     * 
+     * @var string
+     */
+    private $sql_delete;
+    
+    /**
+     * The configuration callback for data interaction. Should expect SQL
+     * format string from one of the according properties ($sql_select,
+     * $sql_insert, $sql_update, $sql_delete) as first parameter, and args
+     * for filling into the SQL format string by sprintf. Should return
+     * the read data in case of SELECT queries and null in case of data
+     * manipulation queries.
+     *
+     * @var \Closure
+     */
+    private $callback;
+
+}
+
---
+++ src/tests/unit-tests/php/Lousson/Config/Callback/CallbackConfigSQLTest.php
@@ -0,0 +1,234 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 textwidth=75: *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2012 - 2013, The Lousson Project                        *
+ *                                                                       *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * Redistribution and use in source and binary forms, with or without    *
+ * modification, are permitted provided that the following conditions    *
+ * are met:                                                              *
+ *                                                                       *
+ * 1) Redistributions of source code must retain the above copyright     *
+ *    notice, this list of conditions and the following disclaimer.      *
+ * 2) Redistributions in binary form must reproduce the above copyright  *
+ *    notice, this list of conditions and the following disclaimer in    *
+ *    the documentation and/or other materials provided with the         *
+ *    distribution.                                                      *
+ *                                                                       *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   *
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     *
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     *
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE        *
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,            *
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR    *
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)    *
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,   *
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)         *
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED   *
+ * OF THE POSSIBILITY OF SUCH DAMAGE.                                    *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ *  Lousson\Config\Callback\CallbackConfigSQLTest class definition
+ *
+ *  @package    org.lousson.config
+ *  @copyright  (c) 2012 - 2013, The Lousson Project
+ *  @license    http://opensource.org/licenses/bsd-license.php New BSD License
+ *  @author     Nils Gotzhein <nils.gotzhein at gmail.com>
+ *  @filesource
+ */
+namespace Lousson\Config\Callback;
+
+/** Dependencies: */
+use Lousson\Config\AbstractConfigEntityTest;
+use Lousson\Config\Callback\CallbackConfigSQL;
+use Closure;
+
+/**
+ *  A test case for the CallbackConfigSQL implementation
+ *
+ *  @since      lousson/Lousson_Config-0.2.0
+ *  @package    org.lousson.config
+ */
+class CallbackConfigSQLTest extends AbstractConfigEntityTest
+{
+	
+    /**
+     *  Obtain the config entity to test
+     *
+     *  The getConfigEntity() method returns the AnyConfigEntity object
+     *  that is about to be tested.
+     *
+     *	@param	$callback	Closure
+     *  @return \Lousson\Config\AnyConfigEntity
+     *          A config entity is returned on success
+     */
+    public function getConfigEntity( Closure $callback = null )
+    {
+ 		if(null === $callback) {
+    		$callback = $this->getCallback();
+ 		}
+    	
+    	$index = array(
+    		'entity_key' => 'entity_value'
+    	);
+    	
+        $config = new CallbackConfigSQL(
+			$callback,
+			self::TABLE_NAME,
+			self::KEY_FIELD,
+			self::VALUE_FIELD,
+			$index
+		);
+        return $config;
+    }
+
+    /**
+     *  Test the internal error handling
+     *
+     *  The testGetErrorOption() method is a test case that verifies that
+     *  non-config exceptions raised by the callback do not lead to a
+     *  violation of the AnyConfig interface.
+     *
+     *  @expectedException          Lousson\Config\Error\ConfigRuntimeError
+     *  @test
+     *
+     *  @throws \Lousson\Config\Error\ConfigRuntimeError
+     *          Raised in case the test is successful
+     *
+     *  @throws \Exception
+     *          Raised in case of an implementation error
+     */
+    public function testGetErrorOption()
+    {
+        $callback = function($name, $fallback) {
+            throw new \DomainException("foo bar baz");
+        };
+
+        $config = $this->getConfigEntity($callback);
+        $config->getOption("test");
+    }
+    
+    /**
+     *  Test the internal error handling
+     *
+     *  The testSetErrorOption() method is a test case that verifies that
+     *  non-config exceptions raised by the callback do not lead to a
+     *  violation of the AnyConfig interface.
+     *
+     *  @expectedException          Lousson\Config\Error\ConfigRuntimeError
+     *  @test
+     *
+     *  @throws \Lousson\Config\Error\ConfigRuntimeError
+     *          Raised in case the test is successful
+     *
+     *  @throws \Exception
+     *          Raised in case of an implementation error
+     */
+    public function testSetErrorOption()
+    {
+    	$callback = function() {
+    		$parameter = func_get_args();
+    		$format = array_shift($parameter);
+    		$operation = strstr($format, ' ', true);
+    		if('INSERT' === $operation || 'UPDATE' === $operation) {
+    			throw new \DomainException("foo bar baz");
+    		}
+    	};
+    
+    	$config = $this->getConfigEntity($callback);
+    	$config->setOption("test", null);
+    }
+
+    /**
+     *  Test the internal error handling
+     *
+     *  The testHasErrorOption() method is a test case that verifies that
+     *  non-config exceptions raised by the callback do not lead to a
+     *  violation of the AnyConfig interface.
+     *
+     *  @expectedException          PHPUnit_Framework_Error_Warning
+     *  @test
+     *
+     *  @throws \PHPUnit_Framework_Error_Warning
+     *          Raised in case the test is successful
+     *
+     *  @throws \Exception
+     *          Raised in case of an implementation error
+     */
+    public function testHasErrorOption()
+    {
+        $callback = function($name, &$fallback) {
+            throw new \DomainException("foo bar baz");
+        };
+
+        $config = new CallbackConfigSQL($callback);
+        $config->hasOption("test");
+    }
+    
+    /**
+     * Example value for the parameter $tableName of the constructor
+     * of the tested class.
+     *  
+     * @var string
+     */
+    const TABLE_NAME = 'table';
+    
+    /**
+     * Example value for the parameter $keyField of the constructor
+     * of the tested class.
+     * 
+     * @var string
+     */
+    const KEY_FIELD = 'key_field';
+    
+    /**
+     * Example value for the parameter $valueField of the constructor
+     * of the tested class.
+     * 
+     * @var string
+     */
+    const VALUE_FIELD = 'valueField';
+    
+    /**
+     * Method providing a valid callback for passing to the constructor
+     * of the tested class. Analyzes the inpute data and stores it in a
+     * local static cache for testing.
+     * 
+     * @return mixed
+     */
+    final protected function getCallback() {
+    	$test = $this;
+    	return function() use($test) {
+    		static $cache = array();
+    		$parameter = func_get_args();
+    		$format = array_shift($parameter);
+    		$test->assertInternalType("string", $format);
+			$operation = strstr($format, ' ', true);
+			if('INSERT' === $operation || 'UPDATE' === $operation) {
+				$value = array_shift($parameter);
+			}
+
+			$cacheKey = implode( ':', $parameter );
+			
+			if('INSERT' === $operation || 'UPDATE' === $operation) {
+				$cache[$cacheKey] = $value;
+			}
+			elseif('DELETE' === $operation && array_key_exists( $cacheKey, $cache )) {
+				unset( $cache[ $cacheKey ] );
+			}
+			elseif('SELECT' === $operation) {
+				$res = array_key_exists( $cacheKey, $cache ) ? $cache[ $cacheKey ] : null;
+				return $res;
+			}
+			
+			
+    	};
+
+    }
+
+}
+
